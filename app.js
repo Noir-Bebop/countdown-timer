ff92c38855a1334e96747a8b33b79d0b32b1c9af
Noir-Bebop <55720391+Noir-Bebop@users.noreply.github.com>
2023-02-10 18:58:03 -0600

    Creando la función para obtener la fecha el milisegundos

--- app.js
+++ app.js
@@ -17,7 +17,26 @@ const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Frida
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
-console.log(items);
 
-let futureDate = new Date(2023, 4, 24, 17, 30, 0);
-console.log(futureDate);
+let futureDate = new Date(2023, 4, 24, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hour = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const date = futureDate.getDate();
+let month = futureDate.getMonth();
+let weekday = futureDate.getDay();
+
+giveaway.textContent = `giveaway end on ${weekdays[weekday]}, ${date} ${months[month]} ${year}  ${hour}:${minutes}am`;
+
+//futer time in ms
+const futureTime = futureDate.getTime();
+console.log(futureTime);
+
+const getRemainingTime = () => {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  console.log(t);
+};
+
+getRemainingTime();
